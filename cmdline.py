1fc8a6beafa411067ffbf6d2205ea196e9897a8a
yothinix <m@yothinix.com>
2017-05-28 09:18:41 +0700

    Fork result and runner from CPython :rocket:

---
+++ cmdline.py
@@ -0,0 +1,52 @@
+import unittest
+import datetime
+from io import StringIO
+from pprint import pprint
+
+import click
+
+from display import (display_status, print_testcase, print_summary,
+                     print_errors, print_failures)
+from runner import TextTestRunner
+from utils import is_pass
+
+
+start_time = datetime.datetime.now()
+
+stream = StringIO()
+runner = TextTestRunner(verbosity=2)
+loader = unittest.TestLoader()
+tests = loader.discover(start_dir='.', pattern='test*.py')
+
+result = runner.run(tests)
+end_time = datetime.datetime.now()
+
+# stream.seek(0)
+# test_result = stream.read()
+# text_line = test_result.split('\n')
+# count = 0
+# pass_count = 0
+# for line in text_line:
+    # if count < result.testsRun:
+        # test_instance = line.split(' ')
+        # if is_pass(test_instance[3]):
+            # pass_count += 1
+
+        # print_testcase(
+            # status=display_status(test_instance[3]),
+            # name=test_instance[0],
+            # suite=test_instance[1]
+        # ) if is_pass(test_instance[3]) else None
+    # count += 1
+
+# print_failures(failures=result.failures)
+# print_errors(errors=result.errors)
+
+# print_summary(
+    # pass_number=pass_count,
+    # fail_number=len(result.failures),
+    # err_number=len(result.failures),
+    # total_test=result.testsRun,
+    # execution_time=(end_time - start_time).total_seconds()
+# )
+
--- example_project/test_fizzbuzz.py
+++ example_project/test_fizzbuzz.py
@@ -1,31 +1,37 @@
+import time
 import unittest
 
-from fizzbuzz import fizzbuzz
+from .fizzbuzz import fizzbuzz
 
 
 class TestFizzBuzz(unittest.TestCase):
     def test_input_one_return_one(self):
         expect = '1'
         actual = fizzbuzz(1)
+        time.sleep(1)
         self.assertEqual(actual , expect)
 
     def test_input_three_return_fizz(self):
         expect = 'fizz'
         actual = fizzbuzz(3)
+        time.sleep(1)
         self.assertEqual(actual , expect)
 
     def test_input_five_return_buzz(self):
         expect = 'buzz'
+        time.sleep(1)
         actual = fizzbuzz(5)
         self.assertEqual(actual , expect)
 
     def test_input_six_return_fizz(self):
         expect = 'fizz'
+        time.sleep(1)
         actual = fizzbuzz(6)
         self.assertEqual(actual , expect)
 
     def test_input_fifthteen_return_fizzbuzz(self):
         expect = 'fizzbuzz'
+        time.sleep(1)
         actual = fizzbuzz(15)
         self.assertEqual(actual , expect)
 
